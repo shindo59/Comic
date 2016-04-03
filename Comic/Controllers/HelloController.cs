00ae2ec47f3f569ec74557047fa36cd133af820a
OP-Aokiji <huynh.duc.khoa02@gmail.com>
2016-04-03 14:56:25 +0700

    2016/03/04

---
+++ Comic/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Comic.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Hello Page";
+            return View();
+        }
+
+    }
+}
